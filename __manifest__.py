9767886e08539b725a8aa1d2f28bf01c832d830f
Osman Lenin <olgsevilla@gmail.com>
2021-01-02 22:32:50 -0600

    addming custom api

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom api Module",
+
+    'summary': """
+        Module to get custom apis""",
+
+    'description': """
+        Creating custom adoo api
+    """,
+
+    'author': "Osman",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Dev',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+}
