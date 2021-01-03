3d7f2bc0d5ba84e9aebb1d2cc33510e5dee4aeaf
Osman Lenin <olgsevilla@gmail.com>
2021-01-02 22:51:59 -0600

    adding custom api as module

---
+++ custom-api-module/controllers/controllers.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+class CustomAPI(http.Controller):
+    @http.route('/custom_api/get_products',  auth='public')
+    def get_products(self):
+        print('entro a al servicio')
+        product_rec = request.env['product.product'].search([])
+        products =[]
+        for rec in product_rec:
+            vals = {
+                'id' = rec.id,
+                'code' = rec.code,
+                'name' = rec.name
+                
+            }
+           
+            products.add(vals)
+        print('Lista =>',products)
+        data = {'status':200,'response':products, 'message':'Success'}
+        return data
+
+  
